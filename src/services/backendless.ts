a8360f16990ebc82c4b34a02ff1a87b2459238fd
Christophorus Rinovan <christophorusrinovan@gmail.com>
2026-06-07 22:42:08 +0700

    repairing api

--- src/services/backendless.ts
+++ src/services/backendless.ts
@@ -3,6 +3,9 @@ import Backendless from "backendless";
 const APP_ID = import.meta.env.VITE_BACKENDLESS_APP_ID;
 const API_KEY = import.meta.env.VITE_BACKENDLESS_API_KEY;
 
+console.log("Backendless APP_ID ada?", Boolean(APP_ID));
+console.log("Backendless API_KEY ada?", Boolean(API_KEY));
+
 Backendless.initApp(APP_ID, API_KEY);
 
 export default Backendless;
